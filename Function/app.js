1e4bed465ba36771fe5792b7442ff9118702492a
sharp231 <yuudai07ya@gmail.com>
2024-04-10 22:17:24 +0900

    return

--- Function/app.js
+++ Function/app.js
@@ -21,17 +21,24 @@
 //     console.log('ミ');
 // }
 
-function greet(fisrtName, lastName) {
-  console.log(`Hi, ${fisrtName} ${lastName[0]}.`);
-}
+// function greet(fisrtName, lastName) {
+//   console.log(`Hi, ${fisrtName} ${lastName[0]}.`);
+// }
 // greet('ken', 'yamada');
 
-repeat('hi', 3);
+// repeat('hi', 3);
 
-function repeat(str, numTimes) {
-  let result = '';
-  for (let i = 0; i < numTimes; i++) {
-    result += str;
+// function repeat(str, numTimes) {
+//   let result = '';
+//   for (let i = 0; i < numTimes; i++) {
+//     result += str;
+//   }
+//   console.log(result);
+// }
+
+function add(x,y){
+  if(typeof x !== 'number' || typeof y !== 'number'){
+    return false;
   }
-  console.log(result);
+  return x + y;
 }
