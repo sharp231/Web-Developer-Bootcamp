e9cb48a0119133128477ca4a844b5a7a37600eae
sharp231 <yuudai07ya@gmail.com>
2024-04-19 14:24:25 +0900

    scope

---
+++ scope/app.js
@@ -0,0 +1,56 @@
+// let total = 0;
+// function collect() {
+//   total = 6;
+// }
+// console.log(total);
+// collect();
+// console.log(total);
+
+// let bird = 'アオサギ';
+// function birdwatch() {
+//     let bird = 'ムクドリ';
+//     console.log(bird);
+// }
+// birdwatch();
+// console.log(bird);
+
+// let radius = 8;
+// if (radius > 0) {
+//   const PI = 3.14;
+//   let msg = 'hogehoge';
+//   console.log(PI);
+// }
+// console.log(radius);
+// console.log(msg);
+
+// for(let i = 0; i < 5; i++) {
+//     let msg = 'al;skdj;fa;sdlkfj';
+// }
+// console.log(msg);
+// console.log(i);
+
+// function bankRobbery() {
+//   const hereoes = ['スパイダーマン','スーパーマン','ブラック・パンサー'];
+//   function help() {
+//     for (let hero of hereoes) {
+//       console.log(`助けて、${hero}`);
+//     }
+//   }
+//   help();
+// }
+
+// let x = 'あいうえお';
+// function hoge () {
+//     console.log(x);
+// }
+// function moge(){
+//     let x = 'かきくけこ';
+//     hoge();
+// }
+
+// function add(a, b) {
+//     return a + b;
+// }
+const add = function(a, b) {
+    return a + b;
+};
