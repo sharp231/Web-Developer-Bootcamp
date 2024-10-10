85a88d27ee7da82c27ba4cff7d65c07c8f932c67
sharp231 <yuudai07ya@gmail.com>
2024-10-11 00:14:22 +0900

    🚀

---
+++ Async_Errors/index.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const AppError = require('./AppError');
+
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand2', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB Connected OK');
+    })
+    .catch(err => {
+        console.log('MongoDB Error connecting ');
+        console.log(err);
+    });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
+
+const categories = ['fruit', 'dairy', 'vegetable', 'bakery'];
+
+app.get('/products', wrapAsync(async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'all' });
+    }
+}));
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { categories });
+});
+
+app.post('/products', wrapAsync(async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+
+}));
+
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e));
+    }
+}
+
+app.get('/products/:id', wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+        throw new AppError('Product Not Found', 404);
+    }
+    res.render('products/show', { product });
+}));
+
+app.get('/products/:id/edit', wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+        throw new AppError('Product Not Found', 404);
+    }
+    res.render('products/edit', { product, categories });
+}));
+
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+}));
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+});
+
+const handleValidationErr = err => {
+    console.dir(err);
+    return new AppError(`Validation Failed...${err.message}`, 400);
+}
+
+app.use((err, req, res, next) => {
+    // const { status = 500, message = 'Something went wrong' } = err;
+    // res.status(status).send(message);
+    console.log(err.name)
+    if (err.name === 'ValidationError') {
+        err = handleValidationErr(err);
+    }
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
---
+++ Async_Errors/seeds.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand2', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB Connected OK');
+    })
+    .catch(err => {
+        console.log('MongoDB Error connecting ');
+        console.log(err);
+    });
+
+// const p = new Product({ name: 'banana', price: 199, category: 'fruit' });
+
+// p.save().then(p => {
+//     console.log(p);
+// }).catch(e => {
+//     console.log(e);
+// });
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 98,
+        category: 'vegetable'
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 480,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 380,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Celery',
+        price: 198,
+        category: 'vegetable'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 298,
+        category: 'dairy'
+    },
+];
+
+Product.insertMany(seedProducts)
+.then(res => {
+    console.log(res);
+}).catch(e => {
+    console.log(e);
+});
