3d00956a2cf568967066e103722dfe24bbf6fae7
sharp231 <yuudai07ya@gmail.com>
2024-10-12 11:53:24 +0900

    🚧

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -19,6 +20,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
