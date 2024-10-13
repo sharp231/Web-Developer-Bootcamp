042437a3e4d03426e9a7d93c5bb8194e6a55997d
sharp231 <yuudai07ya@gmail.com>
2024-10-14 08:48:32 +0900

    🚀

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -76,8 +76,9 @@ app.all('*', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = '問題がある' } = err;
-    res.status(statusCode).send(message);
+    const { statusCode = 500} = err;
+    if (!err.message) err.message = 'No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
