28204e08437494a8cb717f1d8de89946af62dfec
sharp231 <yuudai07ya@gmail.com>
2024-12-01 12:02:25 +0900

    🚀

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
@@ -11,7 +13,8 @@ const reviewRoutes = require('./routes/reviews');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify:false
 })
     .then(() => {
         console.log('Mongo Connection Open!')
@@ -30,12 +33,34 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-app.use('/campgrounds', campgroundRouters);
-app.use('/campgrounds/:id/reviews', reviewRoutes);
+
+
+const sessionConfig = {
+    secret: 'Mysecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.use('/campgrounds', campgroundRouters);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
