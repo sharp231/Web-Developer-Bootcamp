248b396ca329755575f23c2b52563e092e323505
yudai <yuudai07ya@gmail.com>
2026-01-02 16:57:00 +0900

    ⚡️

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -13,12 +12,19 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp',
+const MongoStore = require('connect-mongo').default;
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -41,14 +47,32 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_',
+}));
+
+const secret = process.env.SECRET || 'mysecret';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: { secret: 'secret' },
+    touchAfter: 24 * 3600
+});
+
+store.on('error', e => {
+    console.log('セッションストアエラー', e);
+});
 
 const sessionConfig = {
-    secret: 'mysecret',
+    store,
+    name: 'session',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 };
@@ -61,9 +85,44 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    'https://cdn.jsdelivr.net',
+    'https://cdn.maptiler.com'
+];
+const styleSrcUrls = [
+    'https://cdn.jsdelivr.net',
+    'https://cdn.maptiler.com'
+];
+const connectSrcUrls = [
+    'https://api.maptiler.com',
+    'https://*.maptiler.com',
+    'https://cdn.jsdelivr.net'
+];
+const fontSrcUrls = []
+const imgSrcUrls = [
+    `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+    'https://images.unsplash.com',
+    'https:',  // すべてのHTTPS画像を許可（推奨）
+];
+
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        childSrc: ["blob:"],
+        objectSrc: [],
+        imgSrc: ["'self'", 'blob:', 'data:', ...imgSrcUrls],
+        fontSrc: ["'self'", ...fontSrcUrls]
+    }
+}));
 
 app.use((req, res, next) => {
-    console.log(req.session);
+    // console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
@@ -90,6 +149,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('ポート3000でリクエスト待受中...');
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`ポート${port}でリクエスト待受中...`);
 });
